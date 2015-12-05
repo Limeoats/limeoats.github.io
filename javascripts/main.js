4078121076e394e4ae66ded392cde4904cd16f9e
Limeoats <admin@limeoats.com>
2015-12-05 17:13:46 -0500

    added disqus

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,3 +2,12 @@ window.onload = function() {
   document.getElementById("currentYear").innerHTML = "-" +
     new Date().getFullYear();
 }
+
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+
+s.src = '//limeoats.disqus.com/embed.js';
+
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
