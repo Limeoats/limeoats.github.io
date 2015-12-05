27f73f0f4e739cabc7ad808bd43ecc7bab64df17
Limeoats <admin@limeoats.com>
2015-12-05 17:15:53 -0500

    fixed broken disqus

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,12 +2,3 @@ window.onload = function() {
   document.getElementById("currentYear").innerHTML = "-" +
     new Date().getFullYear();
 }
-
-(function() { // DON'T EDIT BELOW THIS LINE
-var d = document, s = d.createElement('script');
-
-s.src = '//limeoats.disqus.com/embed.js';
-
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
-})();
