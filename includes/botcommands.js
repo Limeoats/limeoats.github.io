b1939df64e3b1aa14c8da98937630e9af2706520
Limeoats <admin@limeoats.com>
2016-08-17 18:09:39 -0400

    added bot commands

---
+++ includes/botcommands.js
@@ -0,0 +1,16 @@
+[
+	{
+		'Id' : 1,
+		'Command' : '!time',
+		'Response' : 'It is currently {time.strftime(\"%I:%M %p %Z on %A, %B %d, %Y.\")})',
+		'Description' : 'Gets the current time',
+		'Op' : '0'
+	},
+	{
+		'Id' : 2,
+		'Command' : '!messages',
+		'Response' : 'Please give Limeoats a follow at twitter.com/limeoats! All follows are greatly appreciated!!~Go to patreon.com/limeoats to support me! :)',
+		'Description' : 'Important stream messages',
+		'Op' : '1'
+	}
+]
