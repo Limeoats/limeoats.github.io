dd9a13982b7fa1368f58f48c50c690ca5f0b8cb6
Limeoats <admin@limeoats.com>
2016-08-17 19:24:05 -0400

    update limeoatsbot commands

--- includes/botcommands.js
+++ includes/botcommands.js
@@ -11,13 +11,20 @@
 		'Command' : '!messages',
 		'Response' : 'Please give Limeoats a follow at twitter.com/limeoats! All follows are greatly appreciated!!~Go to patreon.com/limeoats to support me! :)',
 		'Description' : 'Important stream messages',
-		'Op' : '1'
+		'Op' : '0'
 	},
 	{
 		'Id' : 3,
 		'Command' : '!commands',
 		'Response' : 'Go to http://www.limeoats.com/limeoatsbot.html to view a list of commands that I will respond to!',
 		'Description' : 'Displays the link to the commands page',
+		'Op' : '0'
+	},
+	{
+		'Id' : 4,
+		'Command' : '!lime2d',
+		'Response' : 'Have you ever wanted a free in-game map/animation editor for C++? Go to http://www.limeoats.com/lime2d for more information!',
+		'Description' : 'Information for Lime2D',
 		'Op' : '0'
 	}
 ]
