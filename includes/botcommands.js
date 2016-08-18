0665a0d4ec82ad58574de6d6f9ced5743ce26992
Limeoats <admin@limeoats.com>
2016-08-18 16:56:12 -0400

    update bot commands

--- includes/botcommands.js
+++ includes/botcommands.js
@@ -1,9 +1,23 @@
 [
 	{
-		'Id' : 1,
-		'Command' : '!time',
-		'Response' : 'It is currently {time.strftime(\"%I:%M %p %Z on %A, %B %d, %Y.\")})',
-		'Description' : 'Gets the current time',
+		'Id' : 3,
+		'Command' : '!commands',
+		'Response' : 'Go to http://www.limeoats.com/limeoatsbot.html to view a list of commands that I will respond to!',
+		'Description' : 'Displays the link to the commands page',
+		'Op' : '0'
+	},
+	{
+		'Id' : 5,
+		'Command' : '!leave',
+		'Response' : 'Okay, bye everyone! {Disconnect}',
+		'Description' : 'Makes LimeoatsBot disconnect from the channel',
+		'Op' : '1'
+	},
+	{
+		'Id' : 4,
+		'Command' : '!lime2d',
+		'Response' : 'Have you ever wanted a free in-game map/animation editor for C++? Go to http://www.limeoats.com/lime2d for more information!',
+		'Description' : 'Information for Lime2D',
 		'Op' : '0'
 	},
 	{
@@ -14,17 +28,10 @@
 		'Op' : '0'
 	},
 	{
-		'Id' : 3,
-		'Command' : '!commands',
-		'Response' : 'Go to http://www.limeoats.com/limeoatsbot.html to view a list of commands that I will respond to!',
-		'Description' : 'Displays the link to the commands page',
-		'Op' : '0'
-	},
-	{
-		'Id' : 4,
-		'Command' : '!lime2d',
-		'Response' : 'Have you ever wanted a free in-game map/animation editor for C++? Go to http://www.limeoats.com/lime2d for more information!',
-		'Description' : 'Information for Lime2D',
+		'Id' : 1,
+		'Command' : '!time',
+		'Response' : 'It is currently {time.strftime(\"%I:%M %p %Z on %A, %B %d, %Y.\")})',
+		'Description' : 'Gets the current time',
 		'Op' : '0'
 	}
 ]
