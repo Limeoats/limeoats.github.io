6af5af0782a78dd82fdf3f327c801297b6746503
cw1650 <admin@limeoats.com>
2017-02-13 12:03:08 -0500

    added a note for lime reimagined to homepage

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 window.onload = function() {
-  document.getElementById("currentYear").innerHTML = "-" + new Date().getFullYear();
+    document.getElementById("currentYear").innerHTML = "-" + new Date().getFullYear();
 
-  //Create links at the top of each page
-  this.createLinks();  
+    //Create links at the top of each page
+    this.createLinks();  
 }
 
 var createLinks = function() {
-  document.getElementsByClassName("project-icons")[0].innerHTML = 
+    document.getElementsByClassName("project-icons")[0].innerHTML = 
     " <a href=\"https://www.twitter.com/limeoats\" target=\"_blank\"><img src=\"images/twitter-icon.png\" title=\"Twitter\" /></a>" + 
       "<a href=\"https://www.youtube.com/channel/UC1azHQrx_NgGnEv5KUZB2jA\" target=\"_blank\"><img src=\"images/youtube-icon.png\" title=\"Youtube\" /></a>" +
       "<a href=\"https://www.patreon.com/limeoats\" target=\"_blank\"><img src=\"images/patreon-icon.png\" title=\"Patreon\" id=\"patreonButton\" /></a>" +
@@ -16,13 +16,13 @@ var createLinks = function() {
 }
 
 function loadJSON(file, callback) {
-  var xobj = new XMLHttpRequest();
-  xobj.overrideMimeType("application/json");
-  xobj.open("GET", file, true);
-  xobj.onreadystatechange = function() {
-    if (xobj.readyState == 4 && xobj.status == "200") {
-      callback(xobj.responseText);
-    }
-  };
-  xobj.send(null);
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open("GET", file, true);
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+        }   
+    };
+    xobj.send(null);
 }
