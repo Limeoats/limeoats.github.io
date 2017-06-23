511ab35abee9cbb8568bea9b84234ae72808530c
cw1650 <admin@limeoats.com>
2017-06-23 15:35:15 -0400

    fix jquery errors

--- js/main.js
+++ js/main.js
@@ -3,36 +3,6 @@ window.onload = function () {
 
     //Create links at the top of each page
     this.createLinks();
-
-    $("#homepageSelector").find("div").click(function() {
-        $(".selected").removeClass("selected");
-        $(this).addClass("selected");
-
-        if ($("#homepageSelector").find("#projects").hasClass("selected")) {
-            $("#mainBlog").hide();
-            $("#mainPublications").hide();
-            $("#mainVideos").hide();
-            $("#mainProjects").fadeIn(100);
-        }
-        else if ($("#homepageSelector").find("#blog").hasClass("selected")) {
-            $("#mainProjects").hide();
-            $("#mainPublications").hide();
-            $("#mainVideos").hide();
-            $("#mainBlog").fadeIn(100);
-        }
-        else if ($("#homepageSelector").find("#publications").hasClass("selected")) {
-            $("#mainProjects").hide();
-            $("#mainBlog").hide();
-            $("#mainVideos").hide();
-            $("#mainPublications").fadeIn(100);
-        }
-        else if ($("#homepageSelector").find("#videos").hasClass("selected")) {
-            $("#mainProjects").hide();
-            $("#mainBlog").hide();
-            $("#mainPublications").hide();
-            $("#mainVideos").fadeIn(100);
-        }
-    });
 }
 
 var createLinks = function () {
